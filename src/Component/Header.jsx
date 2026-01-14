29818d03435b2605612436b94e52cb02731afce8
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2026-01-14 11:20:36 -0800

    Added paragraph in header page

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -9,6 +9,13 @@ const Header = () => {
       id="#header"
     >
       <Navbar />
+      <div className="m-auto text-center text-white leading-10 text-xl md:text-2xl font-bold">
+        <p>We care for yor garden like it's our own.</p>
+        <p>
+          Turning every space into a blooming garden, growing beauty and
+          naturally
+        </p>
+      </div>
     </div>
   );
 };
