f713e3a908b5a325a453b39c3e2598574e3c6485
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2026-01-19 12:06:02 -0800

    Added map function

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { IoPlayBackOutline } from "react-icons/io5";
 import { TbPlayerTrackNext } from "react-icons/tb";
-import project1 from "../assets/project1.jpeg"
-import project2 from "../assets/project2.jpeg"
-import project3 from "../assets/project3.jpeg"
-import project4 from "../assets/project4.jpeg"
-import project5 from "../assets/project5.jpeg"
-import project6 from "../assets/project6.jpeg"
+import project1 from "../assets/project1.jpeg";
+import project2 from "../assets/project2.jpeg";
+import project3 from "../assets/project3.jpeg";
+import project4 from "../assets/project4.jpeg";
+import project5 from "../assets/project5.jpeg";
+import project6 from "../assets/project6.jpeg";
 
 const Project = () => {
-
-  const projects = [
+  const projectData = [
     {
       title: "lawn and flowers",
       location: "Pinnock Estate lekki",
@@ -45,8 +44,8 @@ const Project = () => {
       title: "Pot and Flowers",
       location: "1004 VI Lagos",
       image: project6,
-    }
-  ]
+    },
+  ];
   return (
     <div
       className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
@@ -77,8 +76,18 @@ const Project = () => {
         />
       </div>
       {/* Project slider container */}
-      <div>
-        <div></div>
+      <div className="overflow-hidden">
+        <div className="flex gap-8 transition-transform duration-500 ease-in-out">
+          {projectData.map((projects, index) => (
+            <div key={index} className="relative flex shrink-0 w-full sm:w-1/4">
+              <img
+                className="w-100 h-100"
+                src={projects.image}
+                alt={projects.title}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
