9629e183642f60d8b63536ac2ecf22721b1904d3
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2026-01-19 11:44:30 -0800

    Add project image

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,8 +1,52 @@
 import React from "react";
 import { IoPlayBackOutline } from "react-icons/io5";
 import { TbPlayerTrackNext } from "react-icons/tb";
+import project1 from "../assets/project1.jpeg"
+import project2 from "../assets/project2.jpeg"
+import project3 from "../assets/project3.jpeg"
+import project4 from "../assets/project4.jpeg"
+import project5 from "../assets/project5.jpeg"
+import project6 from "../assets/project6.jpeg"
 
 const Project = () => {
+
+  const projects = [
+    {
+      title: "lawn and flowers",
+      location: "Pinnock Estate lekki",
+      image: project1,
+    },
+
+    {
+      title: "lawn and flowers",
+      location: "Shoreline Estate Ikoyi",
+      image: project2,
+    },
+
+    {
+      title: "flowers",
+      location: "Shoreline Estate Ikoyi",
+      image: project3,
+    },
+
+    {
+      title: "Topiary",
+      location: "Lekki",
+      image: project4,
+    },
+
+    {
+      title: "Lawn and flowers",
+      location: "Igando Lagos",
+      image: project5,
+    },
+
+    {
+      title: "Pot and Flowers",
+      location: "1004 VI Lagos",
+      image: project6,
+    }
+  ]
   return (
     <div
       className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
@@ -10,7 +54,7 @@ const Project = () => {
     >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
         projects
-        <span className="underline-offset-8 decoration-1 underline font-light capitalize">
+        <span className="ml-2 underline-offset-8 decoration-1 underline font-light capitalize">
           completed
         </span>
       </h1>
@@ -34,7 +78,7 @@ const Project = () => {
       </div>
       {/* Project slider container */}
       <div>
-        
+        <div></div>
       </div>
     </div>
   );
