ca846d37871181a78ea8979b08d28339327c7d55
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2026-01-20 10:14:28 -0800

    created slides in project

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoPlayBackOutline } from "react-icons/io5";
 import { TbPlayerTrackNext } from "react-icons/tb";
 import project1 from "../assets/project1.jpeg";
@@ -11,19 +11,19 @@ import project6 from "../assets/project6.jpeg";
 const Project = () => {
   const projectData = [
     {
-      title: "lawn and flowers",
+      title: "lawn And Flowers",
       location: "Pinnock Estate lekki",
       image: project1,
     },
 
     {
-      title: "lawn and flowers",
+      title: "lawn And Flowers",
       location: "Shoreline Estate Ikoyi",
       image: project2,
     },
 
     {
-      title: "flowers",
+      title: "Flowers",
       location: "Shoreline Estate Ikoyi",
       image: project3,
     },
@@ -35,17 +35,44 @@ const Project = () => {
     },
 
     {
-      title: "Lawn and flowers",
+      title: "Lawn And Flowers",
       location: "Igando Lagos",
       image: project5,
     },
 
     {
-      title: "Pot and Flowers",
+      title: "Pot And Flowers",
       location: "1004 VI Lagos",
       image: project6,
     },
   ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const [cardsShow, setCardsShow] = useState(1);
+
+  const nextproject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectData.length);
+  };
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectData.length - 1 : prevIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const updateCardsShow = () => {
+      if (window.innerWidth >= 1024) {
+        setCardsShow(projectData.length);
+      } else {
+        setCardsShow(1);
+      }
+    };
+    updateCardsShow();
+
+    window.addEventListener("resize", updateCardsShow);
+    return () => window.removeEventListener("resize", updateCardsShow);
+  }, []);
   return (
     <div
       className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
@@ -66,25 +93,40 @@ const Project = () => {
       {/* sliders button */}
       <div className="flex justify-end items-center mb-8 text-4xl">
         <IoPlayBackOutline
+          onClick={prevProject}
           className="p-3 rounded mr-2 bg-gray-200 cursor-pointer"
           aria-label="Previous Project"
         />
 
         <TbPlayerTrackNext
+          onClick={nextproject}
           className="p-3 rounded mr-2 bg-gray-200 cursor-pointer"
           aria-label="Next Project"
         />
       </div>
       {/* Project slider container */}
       <div className="overflow-hidden">
-        <div className="flex gap-8 transition-transform duration-500 ease-in-out">
+        <div
+          className="flex gap-8 transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${(currentIndex * 100) / cardsShow}%)`,
+          }}
+        >
           {projectData.map((projects, index) => (
             <div key={index} className="relative flex shrink-0 w-full sm:w-1/4">
               <img
-                className="w-100 h-100"
+                className="w-100 h-auto mb-14"
                 src={projects.image}
                 alt={projects.title}
               />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 py-2 shadow-md text-center">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {projects.title}
+                  </h2>
+                  <h2 className="text-gray-500 text-sm">{projects.location}</h2>
+                </div>
+              </div>
             </div>
           ))}
         </div>
