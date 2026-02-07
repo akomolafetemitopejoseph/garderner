6f9511e03bcafa1895e3fe327c1e6a5efac73c04
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2026-02-07 11:58:23 -0800

    Added react scroll

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import logo from "../assets/joshLogo_modifyCopy.jpeg";
+import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
@@ -11,8 +12,14 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-start mb-5">
         <div>
           <div className="flex justify-center items-center gap-5 capitalize mb-8 md:mb-0">
-          <h1 className="text-black font-extralight text-sm">plant & garden</h1>
-            <img className="w-15 h-15 border-green-700 rounded-full " src={logo} alt="logo" />
+            <h1 className="text-black font-extralight text-sm">
+              plant & garden
+            </h1>
+            <img
+              className="w-15 h-15 border-green-700 rounded-full "
+              src={logo}
+              alt="logo"
+            />
           </div>
 
           <p className="text-gray-200 mt-4 mb-5">
@@ -26,18 +33,19 @@ const Footer = () => {
             Company
           </h3>
           <ul className="flex text-center flex-col gap-2 text-gray-200">
-            <a href="/header" className="hover:text-gray-700">
-              Home
-            </a>
-            <a href="#about" className="hover:text-gray-700">
-              About us
-            </a>
-            <a href="#project" className="hover:text-gray-700">
-              project
-            </a>
-            <a href="#contact" className="hover:text-gray-700">
-              Contact
-            </a>
+            <Link to="/header" smooth={true} duration={500}>
+              <li className="hover:text-gray-700">Home</li>
+            </Link>
+
+            <Link to="about" smooth={true} duration={500}>
+              <li className="hover:text-gray-700">About us</li>
+            </Link>
+            <Link to="project" smooth={true} duration={500}>
+              <li className="hover:text-gray-700">Project</li>
+            </Link>
+            <Link to="contact" smooth={true} duration={500}>
+              <li className="hover:text-gray-700">Contact</li>
+            </Link>
           </ul>
         </div>
         <div>
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div
       className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
       style={{ backgroundImage: `url(${bgImage})` }}
-      id="header"
+      id="/header"
     >
       <Navbar />
       <motion.div
