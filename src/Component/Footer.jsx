94cb973d05aedcc2863824596d680533c6cb5826
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2026-02-12 10:51:00 -0800

    Updated changes in footer component

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
       className="pt-10 px-4 md:px-20 lg:px-32 bg-green-800 w-full overflow-hidden"
       id="footer"
     >
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start mb-5">
+      <div className="container text-center mx-auto flex flex-col md:flex-row justify-between items-start mb-5">
         <div>
           <div className="flex justify-center items-center gap-5 capitalize mb-8 md:mb-0">
-            <h1 className="text-black font-extralight text-sm">
+            <h1 className="text-black font-extralight text-lg">
               Josh plant & garden
             </h1>
             <img
@@ -22,14 +22,14 @@ const Footer = () => {
             />
           </div>
 
-          <p className="text-gray-200 mt-4 mb-5">
+          <p className="text-gray-200 mt-4 mb-5 text-2xl">
             Reliable flower landscaping plant care <br /> garden maintenance
             services you can trust.
           </p>
         </div>
 
         <div className="w-full md:w-1/5 mb-8 md:mb-0">
-          <h3 className="text-black text-2xl font-extrabold mb-4 text-center">
+          <h3 className="text-black text-3xl font-extrabold mb-4 text-center">
             Company
           </h3>
           <ul className="flex text-center flex-col gap-2 text-gray-200">
@@ -48,8 +48,8 @@ const Footer = () => {
             </Link>
           </ul>
         </div>
-        <div>
-          <h1 className="text-[18px] lg:text-sm">
+        <div className="flex justify-center">
+          <h1 className="text-[15px] lg:text-sm">
             Copyright 2026 &copy;. All Right Reserved
           </h1>
         </div>
